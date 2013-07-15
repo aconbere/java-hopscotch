5e1a75057a05211ec57c1b7cb4e424cec9049e42
Anders Conbere <aconbere@etsy.com>
2013-07-15 10:46:08 -0700

    hopscotch interface setup

---
+++ hopscotch/src/main/java/org/conbere/hopscotch/HopscotchHashMap.java
@@ -0,0 +1,13 @@
+package org.conbere.hopscotch;
+
+import java.util.Map;
+import java.util.Set;
+import java.util.AbstractMap;
+
+public class HopscotchHashMap<K,V> extends AbstractMap<K,V> implements Map<K,V>
+{
+    @Override public Set<Map.Entry<K,V>> entrySet()
+    {
+        return null;
+    }
+}
