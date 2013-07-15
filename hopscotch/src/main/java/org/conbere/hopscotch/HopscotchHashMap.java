1120a3b2aa6c880bf27ab60399cc375ecc3cf393
Anders Conbere <aconbere@etsy.com>
2013-07-15 11:10:14 -0700

    store a table

--- hopscotch/src/main/java/org/conbere/hopscotch/HopscotchHashMap.java
+++ hopscotch/src/main/java/org/conbere/hopscotch/HopscotchHashMap.java
@@ -6,8 +6,22 @@ import java.util.AbstractMap;
 
 public class HopscotchHashMap<K,V> extends AbstractMap<K,V> implements Map<K,V>
 {
+    private int DEFAULT_INITIAL_CAPACITY = 16;
+
+    private Entry<K,V>[] table;
+
+    public HopscotchHashMap() {
+        this.table = new Entry [DEFAULT_INITIAL_CAPACITY];
+    }
+
     @Override public Set<Map.Entry<K,V>> entrySet()
     {
         return null;
     }
+
+    // @Override public V put(K key, V value)
+    // {
+    //     this.entries[hashCode(key)] = value;
+    //     return value;
+    // }
 }
