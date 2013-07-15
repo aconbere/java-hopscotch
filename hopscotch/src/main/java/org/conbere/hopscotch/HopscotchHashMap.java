c81caa9d11c21e317bc1ecc282d06542d0aa60e4
Anders Conbere <aconbere@etsy.com>
2013-07-15 12:00:59 -0700

    much more stubbing

--- hopscotch/src/main/java/org/conbere/hopscotch/HopscotchHashMap.java
+++ hopscotch/src/main/java/org/conbere/hopscotch/HopscotchHashMap.java
@@ -7,17 +7,64 @@ import java.util.AbstractMap;
 
 public class HopscotchHashMap<K,V> extends AbstractMap<K,V> implements ConcurrentMap<K,V>
 {
-    private int DEFAULT_INITIAL_CAPACITY = 16;
+    static final int DEFAULT_INITIAL_CAPACITY = 16;
+    static final float DEFAULT_LOAD_FACTOR = 0.75f;
+    static final int DEFAULT_CONCURRENCY_LEVEL = 16;
+    static final int DEFAULT_CACHE_LINE_SIZE = 16;
+    static final boolean DEFAULT_IS_OPTIMIZE_CACHE_LINE = true;
+    static final int DEFAULT_HOP_RANGE = 16;
 
     private Entry<K,V>[] table;
 
-    public HopscotchHashMap() {
-        this.table = new Entry [DEFAULT_INITIAL_CAPACITY];
+    final Segment<K,V>[] segments;
+
+    final int segmentShift;
+    final int segmentMask;
+    final int bucketMask;
+    final int hopRange;
+
+    public HopscotchHashMap(int initialCapacity, int concurrencyLevel, int cacheLineSize, boolean isOptimizeCacheline, int hopRange)
+    {
+        this.table = new Entry [initialCapacity];
+        this.segments = new Segment [initialCapacity];
+        this.segmentShift = 0;
+        this.segmentMask = 0;
+        this.bucketMask = 0;
+
+        this.hopRange = hopRange;
+    }
+
+    public HopscotchHashMap()
+    {
+        this(DEFAULT_INITIAL_CAPACITY, DEFAULT_CONCURRENCY_LEVEL, DEFAULT_CACHE_LINE_SIZE, DEFAULT_IS_OPTIMIZE_CACHE_LINE, DEFAULT_HOP_RANGE);
+    }
+
+    private Segment<K,V> getSegment(int hash)
+    {
+        return null;
+    }
+
+    private Bucket<K,V> getBucket(Segment<K,V> segment, int hash)
+    {
+        return null;
+    }
+
+    private int hash(K key)
+    {
+        return 0;
     }
 
     @Override public Set<Map.Entry<K,V>> entrySet()
     {
         return null;
+    }
+
+    @Override public V put(K key, V value)
+    {
+        int hash = hash(key);
+        Segment<K,V> segment = getSegment(hash);
+        Bucket<K,V> bucket = getBucket(segment, hash);
+        return value;
     }
 
     @Override public V replace(K key, V value)
@@ -40,11 +87,11 @@ public class HopscotchHashMap<K,V> extends AbstractMap<K,V> implements Concurren
         return null;
     }
 
+    private class Segment<K,V>
+    {
+    }
 
-
-    // @Override public V put(K key, V value)
-    // {
-    //     this.entries[hashCode(key)] = value;
-    //     return value;
-    // }
+    private class Bucket<K,V>
+    {
+    }
 }
